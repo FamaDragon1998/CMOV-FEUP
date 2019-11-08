ff3bcc902fc478c759bc358f5fadeb2d0c851334
LastLombax <up201503447@fe.up.pt>
2019-11-08 13:56:31 +0000

    some fixes

--- Server/app.js
+++ Server/app.js
@@ -5,37 +5,12 @@ const bodyParser = require('body-parser')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-//var db = require('./database/db')
-
 
 var indexRouter = require('./routes/index');
 var userRouter = require('./routes/user');
 var productRouter = require('./routes/product');
-var transactionRouter = require('./routes/transaction');
-
-const { User} = require('./sequelize/sequelize')
 
  var app = express();
-/*
-var data = {
-  tables: {
-    user: [
-     {id: 1, username: "John", email: "cenas", password:"coiso", card_number:123123, card_cvs: 101, total_spent: 10, stored_discount:0},
-     {id: 2, username: "Kimbolas", email: "cenas", password:"coiso", card_number:123123, card_cvs: 101, total_spent: 10, stored_discount:0},    ],
-    transaction: [
-      {id: 1, voucher: 745747, total_value: 120, flag: true},
-      {id: 2, voucher: 111111, total_value: 1, flag: false},
-      {id: 3, voucher: null, total_value: 55, flag: false},
-    ],
-  },
-}
-
-db.connect(db.MODE_PRODUCTION, function() {
-  db.fixtures(data, function(err) {
-    if (err) return console.log(err)
-    console.log('Data has been loaded...')
-  })
-}) */
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -51,19 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/user', userRouter);
 app.use('/product', productRouter);
-app.use('/transaction', transactionRouter);
-
-/* db.connect(db.MODE_PRODUCTION, function(err) {
-  if (err) {
-    console.log('Unable to connect to MySQL.')
-    process.exit(1)
-  } else {
-    app.listen(3306, function() {
-      console.log('Listening on port 3306...')
-    })
-  }
-}) */
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- Server/routes/user.js
+++ Server/routes/user.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const {User} = require('../sequelize/models/User')
+const User = require('../sequelize/sequelize.js').User;
+const Transaction = require('../sequelize/sequelize.js').Transaction;
 
-/* Everything related to Users*/
 
 //Register 
 router.post('/register', function(req, res, next) {
@@ -23,8 +23,26 @@ router.get('/:id/vouchers', function(req, res, next) {
 
 //Returns info of User
 router.get('/:name', function(req, res, next) {
-    User.findAll({ where: {name: req.params.name} })
+    User.findOne({ where: {username: req.params.name} })
     .then(users => res.json(users))
-  });
+});
+
+//Returns all transactions of a user
+router.get('/transactions', function(req, res, next) {
+  Transaction.findOne({ where: {user_id: req.body.user_id} })
+    .then(transactions => res.json(transactions))
+});
+
+//Return a transaction of a user
+router.get('/transactions/:id', function(req, res, next) {
+  Transaction.findOne({ where: {user_id: req.body.user_id, transaction_id: req.params.id} })
+    .then(transactions => res.json(transactions))
+});
+
+//Checkout basket
+router.post('/checkout', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
 
 module.exports = router;
