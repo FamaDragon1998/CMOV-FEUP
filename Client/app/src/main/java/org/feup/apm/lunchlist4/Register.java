345227eac54d164460502a6225ae211d8b40611d
Ricardo Lopes (KB) <ricardolopes8e@gmail.com>
2019-11-09 16:48:01 +0000

    register form

---
+++ Client/app/src/main/java/org/feup/apm/lunchlist4/Register.java
@@ -0,0 +1,26 @@
+package org.feup.apm.lunchlist4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import org.feup.apm.lunchlist4.ui.login.LoginActivity;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void backlogin(View view)
+    {
+        startActivity(new Intent(this, LoginActivity.class));
+        overridePendingTransition(R.anim.slide_out_right, R.anim.slide_in_left);
+
+    }
+
+}
