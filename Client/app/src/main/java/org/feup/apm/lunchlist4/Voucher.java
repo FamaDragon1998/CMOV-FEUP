d3d6f8e8818103e69b0ce62216c0865eb77acdc1
LastLombax <up201503447@fe.up.pt>
2019-11-13 15:50:53 +0000

    pqp vitor

--- Client/app/src/main/java/org/feup/apm/lunchlist4/Voucher.java
+++ Client/app/src/main/java/org/feup/apm/lunchlist4/Voucher.java
@@ -1,4 +1,28 @@
 package org.feup.apm.lunchlist4;
 
 public class Voucher {
+
+    private int id;
+    private boolean used;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isUsed() {
+        return used;
+    }
+
+    public void setUsed(boolean used) {
+        this.used = used;
+    }
+
+    public Voucher(int id, boolean used) {
+        this.id = id;
+        this.used = used;
+    }
 }
