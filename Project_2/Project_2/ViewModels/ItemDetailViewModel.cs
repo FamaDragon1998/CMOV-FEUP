6bd73902266cf3aa96ef4751fc0b62031a7a6010
LastLombax <up201503447@fe.up.pt>
2019-11-25 15:52:23 +0000

    added project 2 setup

---
+++ Project_2/Project_2/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Project_2.Models;
+
+namespace Project_2.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ Project_2/Project_2/Views/MenuPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Project_2.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Project_2.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MenuPage : ContentPage
+    {
+        MainPage RootPage { get => Application.Current.MainPage as MainPage; }
+        List<HomeMenuItem> selectedCities;
+
+        public MenuPage()
+        {
+            InitializeComponent();
+
+
+            selectedCities = new List<HomeMenuItem>
+            {
+                new HomeMenuItem {Id = MenuItemType.Browse, Title="Porto" },
+                new HomeMenuItem {Id = MenuItemType.About, Title="Lisboa" },
+
+            };
+
+            ListViewMenu.ItemsSource = selectedCities;
+
+            ListViewMenu.SelectedItem = selectedCities[0];
+
+            ListViewMenu.ItemSelected += async (sender, e) =>
+            {
+                if (e.SelectedItem == null)
+                    return;
+
+                var id = (int)((HomeMenuItem)e.SelectedItem).Id;
+                await RootPage.NavigateFromMenu(id);
+            };
+        }
+
+        private void AddCityButton(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
