1974b89d9c2b79debedf4ef26329378d007af17b
LastLombax <up201503447@fe.up.pt>
2019-12-06 13:34:44 +0000

    started forecast detail page design

--- Project_2/WeatherXamarim/WeatherXamarim/WeatherXamarim/ForecastDetailPage.xaml.cs
+++ Project_2/WeatherXamarim/WeatherXamarim/WeatherXamarim/ForecastDetailPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using WeatherApp.Models;
+﻿using System.Diagnostics;
+using WeatherApp.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -9,57 +10,16 @@ namespace WeatherXamarim
     {
         private List info;
 
+        string ForecastDate;
+
         public ForecastDetailPage(List info)
         {
 
             this.info = info;
+            Debug.Write("info", info.wind.deg.ToString());
             InitializeComponent();
 
-            /*    var layout = new StackLayout { Padding = new Thickness(5, 10) };
-                this.Content = layout;*/
-
-            //Title = info.Date + " Forecast";
-            //_saveAddToolBarItem = new ToolbarItem()
-            //{ Text = "Save" };
-
-            /*ToolbarItems.Add(_saveAddToolBarItem);
-            _saveAddToolBarItem.Clicked += _saveAddToolBarItem_Clicked;*/
-            /* Content = new StackLayout
-             {
-                 Children = {
-                     new Label {
-                         Text = "Todo list data goes here",
-                         HorizontalOptions = LayoutOptions.Center,
-                         VerticalOptions = LayoutOptions.CenterAndExpand
-                     },
-                      new Label {
-                          Text = info.weather[0].description,
-                          HorizontalOptions = LayoutOptions.Center,
-                         VerticalOptions = LayoutOptions.CenterAndExpand
-                      },
-                      new Label {
-                          Text = info.main.humidity.ToString(),
-                          HorizontalOptions = LayoutOptions.Center,
-                         VerticalOptions = LayoutOptions.CenterAndExpand
-                      },
-                        new Label {
-                          Text = info.clouds.all.ToString(),
-                          HorizontalOptions = LayoutOptions.Center,
-                         VerticalOptions = LayoutOptions.CenterAndExpand
-                      },
-                      new Label {
-                          Text = info.wind.speed.ToString(),
-                          HorizontalOptions = LayoutOptions.Center,
-                         VerticalOptions = LayoutOptions.CenterAndExpand
-                      },
-                      new Label {
-                          Text = info.wind.deg.ToString(),
-                          HorizontalOptions = LayoutOptions.Center,
-                         VerticalOptions = LayoutOptions.CenterAndExpand
-                      },
-
-                 }
-             };
+            ForecastDate = info.Date + " Forecast";
              /* if (info.rain != null)
                   if (info.rain.__invalid_name__3h > 0)
                   {
